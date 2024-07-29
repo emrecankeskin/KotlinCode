5b65952e4b9c5ad3b4df19f4e7ffd0ed2e265700
emrecankeskin <emrecanks53@hotmail.com>
2024-07-29 10:59:21 +0300

    Question 1395

--- src/main/kotlin/1395. Count Number of Teams.kt
+++ src/main/kotlin/1395. Count Number of Teams.kt
@@ -1,29 +1,35 @@
 class Q1395 {
     fun numTeams(rating: IntArray): Int {
         var ans: Int = 0
+
+
         for( i in 1 until rating.size-1){
             val left = IntArray(2){0}
             val right = IntArray(2){0}
 
+
+            // search before i
             for(j in 0 until i){
 
                 //left less and bigger
                 if(rating[j] < rating[i]){
-                    left[1]++
+                    left[1]++ //less
                 }else{
-                    left[0]++
+                    left[0]++ // bigger
                 }
             }
+            // search after i
             for(k in (i+1) until rating.size){
 
                 //right less and bigger
                 if(rating[k] < rating[i]){
                     right[1]++ // less
                 }else{
-                    right[0]++ // right
+                    right[0]++ // bigger
                 }
             }
-
+            // ascending                   descending
+            // left less * right bigger + left bigger * right less
             ans += left[0]*right[1] + left[1]*right[0]
         }
 
